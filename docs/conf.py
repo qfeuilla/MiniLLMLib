26e8ead5d540dfdf6eced542124ee7a1d03e44cb
Quentin Feuillade--Montixi <quentinfeuillademontixi42@gmail.com>
2025-05-18 19:57:24 +0200

    feat: update docs

---
+++ docs/conf.py
@@ -0,0 +1,30 @@
+import sys
+import os
+from importlib.metadata import version
+
+# Ensure the src directory is on sys.path for autodoc
+sys.path.insert(0, os.path.abspath('../src'))
+
+project = 'MiniLLMLib'
+copyright = '2025, Quentin Feuillade--Montixi'
+author = 'Quentin Feuillade--Montixi'
+
+# Use setuptools_scm to get the version from git tags
+try:
+    release = version("minillmlib")
+except Exception:
+    release = 'unknown'
+
+extensions = [
+    'myst_parser',  # Markdown support
+    # 'sphinx.ext.autodoc',  # Uncomment if you want API docs
+    # 'sphinx.ext.napoleon', # Uncomment for Google/Numpy docstring style
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
